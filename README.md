a1bcf921ff9853cd77925c730606dec19a02a7cf
jackie840129 <jackieliu@media.ee.ntu.edu.tw>
2021-10-28 19:07:16 +0800

    readme

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 1. Put the pretrained model (["backbone.pth"](https://drive.google.com/file/d/19d-Qm-RkBh9E2P1o_ZbdrHAyoZocFZbK/view?usp=sharing)) under the `pretrain/` folder.
 
 ## Training our FedFR
+1. In `config.py`, you should first change the path of `config.rec` and `config.local_rec`
+2. Run with command `sh run.sh`.
 
 ## Generic Evaluation
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2,3 python3 \
-train.py --pretrained_root './pretrain'  --network 'sphnet' --output_dir './ckpt/test' --loss 'CosFace'  \
---batch_size 64 --num_client 40 --client_sampled_ratio 1.0 --lr 0.001 --total_round 12 --local_epoch 1 --fedface --init_fc \
---add_pretrained_data --combine_dataset  --contrastive_bb --return_all --BCE_local #--adaptive_local_epoch
+CUDA_VISIBLE_DEVICES=0,1 python3 \
+train.py --pretrained_root './pretrain'  --network 'sphnet' --output_dir './ckpt/FedFr' --loss 'CosFace'  \
+--batch_size 64 --num_client 40 --client_sampled_ratio 1.0 --lr 0.001 --total_round 20 --local_epoch 10 --fedface \
+--add_pretrained_data --combine_dataset  --contrastive_bb --return_all --BCE_local --adaptive_local_epoch
